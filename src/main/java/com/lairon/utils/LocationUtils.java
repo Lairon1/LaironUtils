33e2b8e8cba9d2195a4c44208dec610623c432a7
Lairon1 <62989407+Lairon1@users.noreply.github.com>
2022-04-12 19:46:56 +0300

    First commit

---
+++ src/main/java/com/lairon/utils/LocationUtils.java
@@ -0,0 +1,52 @@
+package com.lairon.utils;
+
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class LocationUtils {
+
+    public static Collection<Block> blockRadiusSphere(Location loc, int radius) {
+        Collection<Block> blocks = new ArrayList<>();
+        Block center = loc.getBlock();
+        for (int x = -radius; x <= radius; x++) {
+            for (int y = -radius; y <= radius; y++) {
+                for (int z = -radius; z <= radius; z++) {
+                    Block b = center.getRelative(x, y, z);
+                    if (center.getLocation().distance(b.getLocation()) <= radius) {
+                        blocks.add(b);
+                    }
+                }
+
+            }
+        }
+        return blocks;
+    }
+
+    public static Collection<Location> blockRadiusEmptySphere(Location loc, int radius, int space) {
+        Collection<Location> locations = new ArrayList<>();
+        for (int yaw = -180; yaw < 180; yaw += space) {
+            loc.setYaw(yaw);
+            for (int pitch = -90; pitch < 90; pitch += space) {
+                loc.setPitch(pitch);
+                locations.add(loc.clone().add(loc.getDirection().multiply(radius)));
+            }
+        }
+        return locations;
+    }
+
+
+
+    public static ArrayList<Block> blockCube(Location point1, Location point2){
+        ArrayList<Block> blocks = new ArrayList<>();
+        for (int x = point1.getBlockX(); x < point2.getBlockX(); x++)
+            for (int y = point1.getBlockY(); y < point2.getBlockX(); y ++)
+                for (int z = point1.getBlockZ(); z < point2.getBlockZ(); z++)
+                    blocks.add(new Location(point1.getWorld(), x, y, z).getBlock());
+        return blocks;
+    }
+
+
+}
---
+++ src/main/java/com/lairon/utils/WorldEditUtils.java
@@ -0,0 +1,47 @@
+package com.lairon.utils;
+
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.WorldEditException;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
+import com.sk89q.worldedit.function.operation.Operation;
+import com.sk89q.worldedit.function.operation.Operations;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.session.ClipboardHolder;
+import org.bukkit.Location;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class WorldEditUtils {
+
+    public static void setSchematic(Location loc, File file){
+        com.sk89q.worldedit.world.World adaptedWorld = BukkitAdapter.adapt(loc.getWorld());
+        ClipboardFormat format = ClipboardFormats.findByFile(file);
+        try (ClipboardReader reader = format.getReader(new FileInputStream(file))) {
+            Clipboard clipboard = reader.read();
+            try (EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(adaptedWorld,
+                    -1)) {
+                Operation operation = new ClipboardHolder(clipboard).createPaste(editSession)
+                        .to(BlockVector3.at(loc.getBlockX(), loc.getBlockY(),loc.getBlockZ())).ignoreAirBlocks(true).build();
+                try {
+                    Operations.complete(operation);
+                    editSession.flushSession();
+                } catch (WorldEditException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        } catch (FileNotFoundException exx) {
+            exx.printStackTrace();
+        } catch (IOException exxx) {
+            exxx.printStackTrace();
+        }
+    }
+
+}
